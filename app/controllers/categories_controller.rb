27d1046418f7ca60cebec880a5ccb25bf089b9fc
Eric Chu <erickentleongchu@gmail.com>
2016-09-17 22:05:55 +0000

    added edit feature to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,11 +8,34 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update(category_params)
+    if category.save
+      redirect_to root_path, {notice: "Successfully updated category!"}
+    else
+      redirect_to root_path, {alert: "Sorry, could not update category."}
+    end
+  end
+
   def create
     if category = Category.create(category_params)
       redirect_to root_path, {notice: "Successfully created category!"}
     else
-      redirect_to root_path, {notice: "Sorry, could not create category."}
+      redirect_to root_path, {alert: "Sorry, could not create category."}
+    end
+  end
+
+  def delete
+    category = Category.find(params[:id])
+    if category.destroy
+      redirect_to root_path, {notice: "Successfully deleted category!"}
+    else
+      redirect_to root_path, {alert: "Sorry, could not delete category."}
     end
   end
 
