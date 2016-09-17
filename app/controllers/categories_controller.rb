9e93b3637eab4d826e8990271a03d510ff04049c
Eric Chu <erickentleongchu@gmail.com>
2016-09-17 21:30:31 +0000

    added ability to create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,21 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @subcategories = @category.subcategories
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    if category = Category.create(category_params)
+      redirect_to root_path, {notice: "Successfully created category!"}
+    else
+      redirect_to root_path, {notice: "Sorry, could not create category."}
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks"}
   resources :users, only: [:show]
 
-  resources :categories, only: [:show] do
+  resources :categories do
     resources :subcategories do
 
     end
   end
-  
+
   resources :concepts do
   end
 
