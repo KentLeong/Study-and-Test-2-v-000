a15bf741e75e31e576d5c4b822eec57a65328514
Eric Chu <erickentleongchu@gmail.com>
2016-09-17 23:20:22 +0000

    added feature to add subcategories for categories

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -3,4 +3,24 @@ class SubcategoriesController < ApplicationController
     @category = Category.find(params[:category_id])
     @subcategory = Subcategory.find(params[:id])
   end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @subcategory = Subcategory.new
+  end
+
+  def create
+    subcategory = Subcategory.create(subcategory_params)
+    binding.pry
+    if subcategory.save
+      redirect_to category_path(params[:category_id]), {notice: "Successfully created subcategory"}
+    else
+      redirect_to category_path(params[:category_id]), {alert: "Subcategory was not created"}
+    end
+  end
+
+  private
+    def subcategory_params
+      params.require(:subcategory).permit(:name, :category_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   resources :categories do
-    resources :subcategories do
-
-    end
+    resources :subcategories
   end
 
   resources :concepts do
