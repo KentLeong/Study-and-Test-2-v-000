f0b9e131a5289975c139d4dc8aff4725522a6d86
Eric Chu <erickentleongchu@gmail.com>
2016-09-18 22:45:46 +0000

    added permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
+  def admin?
+    if current_user != nil
+      true if current_user.role == "admin"
+    end
+  end
 
+  def mod?
+    if current_user != nil
+      true if current_user.role == "moderator"
+    end
+  end
+
+  def admin_or_mod?
+    admin? || mod?
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :concept
   belongs_to :user
+
+  enum qtype: { multiple_choice: 0, true_false: 1, word_question: 2 }
+
 end
