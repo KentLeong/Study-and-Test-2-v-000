d5726e9137f463e9e98a9463f9e66392563f9707
kentleong <kentleong@github.com>
2016-09-26 02:55:06 -0400

    fixed bugs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,17 +4,22 @@ class QuestionsController < ApplicationController
   def new
     @concept = Concept.find(params[:concept_id])
     @question = Question.new()
+    @randos = ["", "", ""]
   end
 
   def create
     @randos = params[:random_qs][0]
+    if @randos.include? nil
+      redirect_to request.referrer, {alert: "You need at least 2 rando questions!"} if @randos.count < 2
+    end
+
     @question = Question.create(question_params)
+
     @randos.each do |rando|
       params[:rando] = {
         name: rando,
         question_id: @question.id
       }
-      binding.pry
       @question.random_questions << RandomQuestion.create(rando_params)
     end
     RandomQuestion.create()
@@ -23,9 +28,18 @@ class QuestionsController < ApplicationController
 
   def edit
     @concept = @question.concept
+    @randos = @question.random_questions
   end
 
   def update
+    @randos = params[:random_qs][0]
+    @question.random_questions.each_with_index do |rq, index|
+      params[:rando] = {
+        name: @randos["random#{index+1}"],
+        question_id: @question.id
+      }
+      rq.update(rando_params)
+    end
     @question.update(question_params)
     question_action(:save, "update")
   end
