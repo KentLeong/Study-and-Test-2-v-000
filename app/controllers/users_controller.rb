afd3676478293951514699d92258df4e2442daab
kentleong <kentleong@github.com>
2016-09-25 22:00:09 -0400

    refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user)
+    @quizzes = @user.quizzes.order(updated_at: :DESC)
+    @last_quiz = @user.quizzes.order(updated_at: :DESC).limit(1).first
   end
 end
--- db/migrate/20160918143722_create_quizzes.rb
+++ db/migrate/20160918143722_create_quizzes.rb
@@ -3,6 +3,8 @@ class CreateQuizzes < ActiveRecord::Migration[5.0]
     create_table :quizzes do |t|
       t.integer :user_id
       t.integer :concept_id
+      t.integer :percent
+      t.string :grade
       t.text :question_ids, limit: 1333333337
       t.text :user_answers, limit: 1333333337
       t.timestamps
