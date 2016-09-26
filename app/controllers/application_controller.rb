b3506e4230606ec5d862314dd5a14e276bc347eb
kentleong <kentleong@github.com>
2016-09-26 05:00:08 -0400

    refactored

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
   def validate_admin
     redirect_to root_path, {alert: "You don't have access to that."} if current_user == nil || current_user.role != "admin"
   end
-
   protected
 
   def configure_permitted_parameters
